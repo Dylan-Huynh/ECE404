b4e53bbf4795b7f0fdfd05ca197751fbfe7210bb
Dylan-Huynh <huynh38@purdue.edu>
2023-02-02 14:51:11 -0500

    Update mult_inv.py

--- HW3/mult_inv.py
+++ HW3/mult_inv.py
@@ -32,26 +32,20 @@ def MI(num, mod):
 MI(NUM, MOD)
 
 
-'''
+a, b = int(sys.argv[1]), int(sys.argv[2])
 def bmi(a,b, x, y):
     print(a,b)
-    if a == b: 
-        print("a==b")
-        print(str(int(b))) #(A)
-    if int(a) == 0: 
-        print("a=0")
-        print(str(int(b))) #(B)
-    if int(b) == 0: 
-        print("b=0")
-        print(str(int(a))) #(C)
-    if (~a):
-        if (b): #(E)
+    if a == b: return a #(A)
+    if a == 0: return b #(B)
+    if b == 0: return a #(C)
+    if (~a & 1):
+        if (b & 1): #(E)
             print("inner a>b")
             bmi(a >> 1, b, x, y) #(F)
         else: #(G)
             print("innerelse")
             bmi(a >> 1, b >> 1, x, y) << 1 #(H)
-    if (~b): #(I)
+    if (~b & 1): #(I)
         print("~b&1")
         bmi(a, b >> 1, x, y) #(J)
     if (a > b): #(K)
@@ -59,6 +53,4 @@ def bmi(a,b, x, y):
         bmi((a-b) >> 1, b, x, y) #(L)
     print("else")
     ((b-a) >> 1, a, x, y) #(M)
-a, b = BitVector(intVal = int(sys.argv[1])), BitVector(intVal = int(sys.argv[2]))
 bmi(a, b, 0, 1)
-'''
