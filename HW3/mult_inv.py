4ab124d2cb1d96059270d3b88935bc7cf24147c5
Dylan-Huynh <huynh38@purdue.edu>
2023-02-02 15:23:47 -0500

    Update mult_inv.py

--- HW3/mult_inv.py
+++ HW3/mult_inv.py
@@ -13,22 +13,12 @@ def MI(num, mod):
     y, y_old = 1, 0
     while mod:
         q = num # mod
-        remainder - bitRemainder(num, mod)
+        remainder = bitRemainder(num, mod)
         num, mod = mod, bitRemainder(num, mod) #changes to next step i.e. 35, 19 goes to 19, 16
-        tempx = x
-        while (tempx > 0):
-            if (~tempx & 1):
-                q << 1
-            tempx >> x
-        x, x_old = x_old - q, x #linearization, 
-        
-        q = num
-        tempy = y
-        while (tempy > 0):
-            if (~tempy & 1):
-                q << 1
-            tempy >> 1
-        y, y_old = y_old - q, y
+        new_x = bitMultiply(q, x)
+        new_y = bitMultiply(q, y)
+        x, x_old = x_old - new_x, x #linearization, 
+        y, y_old = y_old - new_y, y
     if num != 1:
         print("\nNO MI. However, the GCD of %d and %d is %u\n" % (NUM, MOD, num))
     else:
@@ -37,32 +27,24 @@ def MI(num, mod):
 
 MI(NUM, MOD)
 
-def bitDivide(a, b):
-
 def bitMultiply(a, b):
+    if (b == 0):
+        return a
+    if (~b & 1):
+        return bitMultiply(a, b >> 1)
+    else:
+        return bitMultiply(a << 1, b >> 1)
 
 def bitRemainder(a,b):
-    
-
-a, b = int(sys.argv[1]), int(sys.argv[2])
-def bmi(a,b, x, y):
-    print(a,b)
     if a == b: return a #(A)
     if a == 0: return b #(B)
     if b == 0: return a #(C)
     if (~a & 1):
-        if (b & 1): #(E)
-            print("inner a>b")
-            bmi(a >> 1, b, x, y) #(F)
+        if (b &1): #(E)
+            return bitRemainder(a >> 1, b) #(F)
         else: #(G)
-            print("innerelse")
-            bmi(a >> 1, b >> 1, x, y) << 1 #(H)
+            return bitRemainder(a >> 1, b >> 1) << 1 #(H)
     if (~b & 1): #(I)
-        print("~b&1")
-        bmi(a, b >> 1, x, y) #(J)
+        return bitRemainder(a, b >> 1) #(J)
     if (a > b): #(K)
-        print("a>b")
-        bmi((a-b) >> 1, b, x, y) #(L)
-    print("else")
-    ((b-a) >> 1, a, x, y) #(M)
-bmi(a, b, 0, 1)
+        return b
